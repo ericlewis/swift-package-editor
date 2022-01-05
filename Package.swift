2b00cf30873639238dd7b3e7366d840144c2fac1
Eric Lewis <ericlewis777@gmail.com>
2022-01-05 12:39:28 -0500

    chore: bump argument parser version

--- Package.swift
+++ Package.swift
@@ -58,7 +58,7 @@ if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
         // The 'swift-argument-parser' version declared here must match that
         // used by 'swift-driver', 'sourcekit-lsp', and 'swiftpm'. Please coordinate
         // dependency version changes here with those projects.
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.4.3")),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.0.1")),
         .package(url: "https://github.com/apple/swift-package-manager.git", .branch(relatedDependenciesBranch)),
         .package(url: "https://github.com/apple/swift-syntax.git", .branch(relatedDependenciesBranch))
     ]
